0c928fae5f247ac2453397e9c1e3ae8596244ab4
Jason Eberle <jason@imgix.com>
2015-05-11 18:09:19 -0700

    Add Gruntfile for validating and testing

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    name: '<%= pkg.name %>',
+
+    jshint: {
+        src: 'tasks/*.js',
+        options: {
+            jshintrc: '.jshintrc',
+            reporter: require('reporter-plus/jshint')
+          }
+      },
+    jscs: {
+        src: 'tasks/*.js',
+        options: {
+            config: '.jscsrc',
+            reporter: require('reporter-plus/jscs').path
+          }
+      },
+
+    'chrome-load': {
+        twitter: {
+            reload_pattern: 'https?:\/\/(www\.)?twitter\.com',
+            new_url: 'https://www.twitter.com'
+          },
+        google: {
+            reload_pattern: 'https?:\/\/(\\w+?\.)?google\.com',
+            new_url: 'https://www.google.com'
+          }
+      }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadTasks('tasks');
+};
